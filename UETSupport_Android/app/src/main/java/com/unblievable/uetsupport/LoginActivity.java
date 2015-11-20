9d97b7bd03287448bb67806bd78ad32b98268c01
DucAnh <ducanh54@gmail.com>
2015-11-21 03:37:07 +0700

    update login with server

--- UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/LoginActivity.java
+++ UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/LoginActivity.java
@@ -1,7 +1,234 @@
 package com.unblievable.uetsupport;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+import com.unblievable.uetsupport.common.CommonUtils;
+import com.unblievable.uetsupport.common.UETSupportUtils;
+import com.unblievable.uetsupport.service.CustomAsyncHttpClient;
+import com.unblievable.uetsupport.service.Service;
+
+import org.json.JSONObject;
+import cz.msebera.android.httpclient.Header;
+
 /**
  * Created by DucAnhZ on 20/11/2015.
  */
-public class LoginActivity {
+public class LoginActivity extends FragmentActivity implements View.OnClickListener{
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private Button btnLogin;
+    private TextView tvForgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_login);
+
+        edtUsername = (EditText) findViewById(R.id.edtUsername);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        tvForgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
+
+        btnLogin.setOnClickListener(this);
+        tvForgotPassword.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnLogin) {
+            login(this, edtUsername.getText().toString(), edtPassword.getText().toString(), "demo");
+        }
+        if (v == tvForgotPassword) {
+            forgotPassword(this, "ducanh54@gmail.com");
+            //TODO -> I'm using my email for demo
+        }
+    }
+
+    private void login(final Activity activity, String username, String password, String deviceId) {
+        if(!UETSupportUtils.networkConnected(this)) {
+            return;
+        }
+
+        CustomAsyncHttpClient client = new CustomAsyncHttpClient(activity, "");
+        String url = Service.ServerURL + "/student/login";
+        RequestParams params = new RequestParams();
+        params.put("username", username);
+        params.put("password", password);
+        params.put("deviceId", deviceId);
+
+        client.post(url, params, new TextHttpResponseHandler() {
+            private ProgressDialog progressBar;
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                try {
+                    progressBar = new ProgressDialog(activity);
+                    progressBar.setCancelable(true);
+                    progressBar.setMessage("Loading ...");
+                    progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progressBar.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                if (statusCode == 200) {
+                    try {
+                        JSONObject jObject = new JSONObject(responseString);
+                        String success = CommonUtils.getValidString(jObject.getString("success"));
+                        if ("1".equals(success)) {
+                            Intent intent = new Intent(activity, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            String message = CommonUtils.getValidString(jObject.getString("message"));
+                            CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), message, null);
+                        }
+                    } catch (Exception e) {
+                        CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), e.getMessage(), null);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), statusCode + "\n" + responseString, null);
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                progressBar.dismiss();
+            }
+        });
+
+    }
+
+    private void forgotPassword(final Activity activity, String email) {
+        if(!UETSupportUtils.networkConnected(this)) {
+            return;
+        }
+
+        CustomAsyncHttpClient client = new CustomAsyncHttpClient(this, "");
+        String url = Service.ServerURL + "/student/forgot-password";
+        RequestParams params = new RequestParams();
+        params.put("email", email);
+
+        client.post(url, params, new TextHttpResponseHandler() {
+            private ProgressDialog progressBar;
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                try {
+                    progressBar = new ProgressDialog(activity);
+                    progressBar.setCancelable(true);
+                    progressBar.setMessage("Loading ...");
+                    progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progressBar.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                if (statusCode == 200) {
+                    try {
+                        JSONObject jObject = new JSONObject(responseString);
+                        String success = CommonUtils.getValidString(jObject.getString("success"));
+                        if ("1".equals(success)) {
+                            //TODO -> Show dialog, write email to revice new password
+                        } else {
+                            String message = CommonUtils.getValidString(jObject.getString("message"));
+                            CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), message, null);
+                        }
+                    } catch (Exception e) {
+                        CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), e.getMessage(), null);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), statusCode + "\n" + responseString, null);
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                progressBar.dismiss();
+            }
+        });
+    }
+
+
+    private void getTeacher(final Activity activity) {
+        if(!UETSupportUtils.networkConnected(this)) {
+            return;
+        }
+
+        CustomAsyncHttpClient client = new CustomAsyncHttpClient(this, "");
+        String url = Service.ServerURL + "/student/list";
+
+        client.get(url, new TextHttpResponseHandler() {
+            private ProgressDialog progressBar;
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                try {
+                    progressBar = new ProgressDialog(activity);
+                    progressBar.setCancelable(true);
+                    progressBar.setMessage("Loading ...");
+                    progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progressBar.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                if (statusCode == 200) {
+                    try {
+                        JSONObject jObject = new JSONObject(responseString);
+                        String success = CommonUtils.getValidString(jObject.getString("success"));
+                        if ("1".equals(success)) {
+                            //TODO -> Show dialog, write email to revice new password
+                        } else {
+                            String message = CommonUtils.getValidString(jObject.getString("message"));
+                            CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), message, null);
+                        }
+                    } catch (Exception e) {
+                        CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), e.getMessage(), null);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                CommonUtils.showOkDialog(activity, getResources().getString(R.string.dialog_title_common), statusCode + "\n" + responseString, null);
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                progressBar.dismiss();
+            }
+        });
+    }
+
 }
--- UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/service/CustomAsyncHttpClient.java
+++ UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/service/CustomAsyncHttpClient.java
@@ -8,12 +8,13 @@ import com.loopj.android.http.AsyncHttpClient;
  * Created by DucAnhZ on 20/11/2015.
  */
 public class CustomAsyncHttpClient extends AsyncHttpClient {
-    private final String USERNAME = "unblievable";
+    private final String USERNAME = "unbelievable";
     private final String PASSWORD = "123456";
 
     public CustomAsyncHttpClient(Context context, String tokenHeader) {
         this.addHeader("username", USERNAME);
         this.addHeader("password", PASSWORD);
         this.addHeader("token", tokenHeader);
+        this.addHeader("Accept-Language", "vi");
     }
 }
