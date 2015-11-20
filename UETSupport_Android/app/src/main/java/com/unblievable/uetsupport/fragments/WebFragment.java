a58f3832116a2845262d94cf5c88c6592e2d8c66
namvp_58 <namvp_58@bitbucket.org>
2015-11-21 00:26:03 +0700

    update tab layout

--- UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/fragments/WebFragment.java
+++ UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/fragments/WebFragment.java
@@ -1,9 +1,22 @@
 package com.unblievable.uetsupport.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.unblievable.uetsupport.R;
 
 /**
  * Created by Nam on 11/20/2015.
  */
 public class WebFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_web,container,false);
+        return v;
+    }
 }
