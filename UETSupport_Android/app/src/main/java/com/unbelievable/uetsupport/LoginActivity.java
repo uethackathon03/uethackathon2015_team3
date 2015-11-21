863df2e437dc1673c4c0cd4c76f680e92b18a4d8
DucAnh <ducanh54@gmail.com>
2015-11-21 20:24:14 +0700

    save profile to sdcard

--- UETSupport_Android/app/src/main/java/com/unbelievable/uetsupport/LoginActivity.java
+++ UETSupport_Android/app/src/main/java/com/unbelievable/uetsupport/LoginActivity.java
@@ -2,8 +2,11 @@ package com.unbelievable.uetsupport;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
 import com.unbelievable.uetsupport.common.CommonUtils;
+import com.unbelievable.uetsupport.common.Constant;
 import com.unbelievable.uetsupport.common.UETSupportUtils;
 import com.unbelievable.uetsupport.service.CustomAsyncHttpClient;
 import com.unbelievable.uetsupport.service.Service;
@@ -28,6 +32,8 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
     private EditText edtPassword;
     private Button btnLogin;
     private TextView tvForgotPassword;
+    private SharedPreferences sharedpreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +44,18 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
         edtPassword = (EditText) findViewById(R.id.edtPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         tvForgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
-
+        sharedpreferences = this.getSharedPreferences(
+                Constant.nameSharedPreferences, Context.MODE_PRIVATE);
         btnLogin.setOnClickListener(this);
         tvForgotPassword.setOnClickListener(this);
+        editor = sharedpreferences.edit();
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnLogin) {
-            login(this, edtUsername.getText().toString(), edtPassword.getText().toString(), "demo");
+            login(this, edtUsername.getText().toString(), edtPassword.getText().toString(), Settings.Secure.getString(this.getContentResolver(),
+                    Settings.Secure.ANDROID_ID));
         }
         if (v == tvForgotPassword) {
             forgotPassword(this, "ducanh54@gmail.com");
@@ -90,6 +99,13 @@ public class LoginActivity extends FragmentActivity implements View.OnClickListe
                         JSONObject jObject = new JSONObject(responseString);
                         String success = CommonUtils.getValidString(jObject.getString("success"));
                         if ("1".equals(success)) {
+                            JSONObject jData = new JSONObject(jObject.getString("data"));
+                            editor.putString(Constant.token, jData.getString("token"));
+                            JSONObject jStudent = new JSONObject(jObject.getString("student"));
+                            editor.putString(Constant.username, jData.getString("username"));
+                            editor.putString(Constant.fullname, jData.getString("fullname"));
+                            editor.putString(Constant.email, jData.getString("email"));
+                            editor.commit();
                             Intent intent = new Intent(activity, MainActivity.class);
                             startActivity(intent);
                         } else {
--- UETSupport_Android/app/src/main/java/com/unbelievable/uetsupport/common/Constant.java
+++ UETSupport_Android/app/src/main/java/com/unbelievable/uetsupport/common/Constant.java
@@ -4,4 +4,9 @@ package com.unbelievable.uetsupport.common;
  * Created by DucAnhZ on 20/11/2015.
  */
 public class Constant {
+    public static final String nameSharedPreferences = "nameSharedPreferences";
+    public static final String token = "token";
+    public static final String username = "username";
+    public static final String fullname = "fullname";
+    public static final String email = "email";
 }
