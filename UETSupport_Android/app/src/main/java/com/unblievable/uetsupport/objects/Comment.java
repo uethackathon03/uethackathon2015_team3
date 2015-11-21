4e9938d2fb0acc9d6279547253f3eea367878ff0
namvp_58 <namvp_58@bitbucket.org>
2015-11-21 08:10:14 +0700

    update Social Fragment

---
+++ UETSupport_Android/app/src/main/java/com/unblievable/uetsupport/objects/Comment.java
@@ -0,0 +1,21 @@
+package com.unblievable.uetsupport.objects;
+
+import java.util.Date;
+
+/**
+ * Created by Nam on 11/21/2015.
+ */
+public class Comment {
+    public long commentId;
+    public long threadId;
+    public long userId;
+
+    public String content;
+    public Integer like;
+
+    public Date createdTime;
+    public Date modifiedTime;
+
+    public int avatar;
+    public String userName;
+}
