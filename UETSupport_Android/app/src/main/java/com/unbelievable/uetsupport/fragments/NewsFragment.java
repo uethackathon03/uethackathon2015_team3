a3d3f7bcddc994f6e43acc8a9480208871fec5e3
DucAnh <ducanh54@gmail.com>
2015-11-22 06:00:47 +0700

    update, fix bugs

--- UETSupport_Android/app/src/main/java/com/unbelievable/uetsupport/fragments/NewsFragment.java
+++ UETSupport_Android/app/src/main/java/com/unbelievable/uetsupport/fragments/NewsFragment.java
@@ -1,5 +1,6 @@
 package com.unbelievable.uetsupport.fragments;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,10 +8,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.loopj.android.http.TextHttpResponseHandler;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.unbelievable.uetsupport.MainActivity;
 import com.unbelievable.uetsupport.R;
 import com.unbelievable.uetsupport.adapter.RecruitmentAdapter;
@@ -52,9 +59,8 @@ public class NewsFragment extends Fragment implements View.OnClickListener {
         recruitmentAdapter = new RecruitmentAdapter(getActivity(), annouceArrayList);
         newsListView.setAdapter(newsArrayAdapter);
 
-
-        btNewsSwitch =(Button) v.findViewById(R.id.btNewsSwitch);
-        btrecruitmentSwitch = (Button)v.findViewById(R.id.btAnnouceSwitch);
+        btNewsSwitch = (Button) v.findViewById(R.id.btNewsSwitch);
+        btrecruitmentSwitch = (Button) v.findViewById(R.id.btAnnouceSwitch);
         btNewsSwitch.setOnClickListener(this);
         btrecruitmentSwitch.setOnClickListener(this);
         if (mainActivity.newsArrayList.size() == 0) {
@@ -77,9 +83,10 @@ public class NewsFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
+
     //TODO
     private void parseNewsFromServer() {
-        if(!UETSupportUtils.networkConnected(getActivity())) {
+        if (!UETSupportUtils.networkConnected(getActivity())) {
             return;
         }
 
@@ -138,5 +145,36 @@ public class NewsFragment extends Fragment implements View.OnClickListener {
             }
         });
 
+    }
+
+    private class NewOnItemClickListener implements AdapterView.OnItemClickListener {
+        private DisplayImageOptions option;
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if (position == 0) {
+                view.setEnabled(false);
+            }
+            option = new DisplayImageOptions.Builder()
+                    .showImageForEmptyUri(R.mipmap.ic_launcher)
+                    .showImageOnFail(R.mipmap.ic_launcher)
+                    .showImageOnLoading(R.mipmap.ic_launcher)
+                    .imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2)
+                    .cacheInMemory(true).cacheOnDisk(true).build();
+
+            Dialog dialog = new Dialog(getActivity());
+            dialog.setContentView(R.layout.news_dialog);
+            dialog.setTitle(mainActivity.newsArrayList.get(position).title);
+            ImageView imvDialogPhoto = (ImageView) dialog.findViewById(R.id.imvDialogPhoto);
+            TextView tvDialogContent = (TextView) dialog.findViewById(R.id.tvDialogContent);
+            try {
+                ImageLoader.getInstance().displayImage(mainActivity.newsArrayList.get(position).photo, imvDialogPhoto, option);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            tvDialogContent.setText(mainActivity.newsArrayList.get(position).content);
+            dialog.show();
+
+        }
     }
 }
